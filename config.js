15308961c1f859e885ddad0bd65ddc796767b9ac
Ajea Smith <smithajea@rocketmail.com>
2021-02-06 15:53:34 -0500

    removed contact me page

--- config.js
+++ config.js
@@ -1,50 +1,47 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by Ajea Smith',
-  subtitle: 'Front-End related Dev Posts',
-  copyright: '© All rights reserved.',
-  disqusShortname: '',
+  url: "https://lumen.netlify.com",
+  pathPrefix: "/",
+  title: "Blog by Ajea Smith",
+  subtitle: "Front-End related Dev Posts",
+  copyright: "© All rights reserved.",
+  disqusShortname: "",
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: "UA-73379983-2",
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
-      path: '/'
+      label: "Articles",
+      path: "/",
     },
     {
-      label: 'About me',
-      path: '/pages/about'
+      label: "About me",
+      path: "/pages/about",
     },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
   ],
   author: {
-    name: 'Ajea Smith',
-    photo: '/ajea.jpg',
-    bio: 'Front-End Developer, building things and writing about it is what I do the most :)',
+    name: "Ajea Smith",
+    photo: "/ajea128x128.jpg",
+    bio:
+      "Front-End Developer, building things and writing about it is what I do the most :)",
     contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-      medium: '',
-    }
-  }
+      email: "",
+      facebook: "#",
+      telegram: "#",
+      twitter: "#",
+      github: "#",
+      rss: "",
+      vkontakte: "",
+      linkedin: "#",
+      instagram: "#",
+      line: "",
+      gitlab: "",
+      weibo: "",
+      codepen: "",
+      youtube: "",
+      soundcloud: "",
+      medium: "",
+    },
+  },
 };
--- src/components/Sidebar/Author/Author.test.js
+++ src/components/Sidebar/Author/Author.test.js
@@ -7,7 +7,7 @@ describe('Author', () => {
   const props = {
     author: {
       name: 'test',
-      photo: '/photo.jpg',
+      photo: '/ajea128x128.jpg',
       bio: 'test'
     },
     isIndex: false
