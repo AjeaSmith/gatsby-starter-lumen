163d11b49d6ed5df2b078176027fcc5d15d50781
Ajea Smith <smithajea@rocketmail.com>
2021-02-06 15:35:13 -0500

    added my image

--- config.js
+++ config.js
@@ -1,41 +1,50 @@
-"use strict";
+'use strict';
 
 module.exports = {
-  url: "https://lumen.netlify.com",
-  pathPrefix: "/",
-  title: "Blog by Ajea Smith",
-  subtitle: "Front-End related Dev Posts",
-  copyright: "© All rights reserved.",
-  disqusShortname: "",
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Ajea Smith',
+  subtitle: 'Front-End related Dev Posts',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: "UA-73379983-2",
+  googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
-      label: "Articles",
-      path: "/",
+      label: 'Articles',
+      path: '/'
     },
     {
-      label: "About me",
-      path: "/pages/about",
+      label: 'About me',
+      path: '/pages/about'
     },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
   ],
   author: {
-    name: "Ajea Smith",
-    photo: "/ajea.jpg",
-    bio:
-      "Front-End Developer, building things and writing about it is what I do the most :)",
+    name: 'Ajea Smith',
+    photo: '/ajea.jpg',
+    bio: 'Front-End Developer, building things and writing about it is what I do the most :)',
     contacts: {
-      email: "smithajea@rocketmail.com",
-      twitter: "#",
-      github: "#",
-      linkedin: "#",
-      instagram: "#",
-      line: "",
-      gitlab: "",
-      weibo: "",
-      youtube: "",
-      medium: "",
-    },
-  },
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    }
+  }
 };
